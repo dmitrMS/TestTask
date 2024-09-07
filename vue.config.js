59ba4730dfbb468728359203e8177ec6a3cf3f14
DmitryMS <dm.zhukov7@mail.ru>
2024-09-07 22:01:33 +0300

    webpack_connect

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,21 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+const path = require('path');
+
+module.exports = {
+  configureWebpack: {
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, 'src'),
+        '@assets': path.resolve(__dirname, 'src/assets'),
+        '@components': path.resolve(__dirname, 'src/components'),
+      },
+    },
+  },
+  chainWebpack: config => {
+    config.module
+      .rule('markdown')
+      .test(/\.md$/)
+      .use('vue-loader')
+      .loader('vue-loader')
+      .end();
+  },
+};
